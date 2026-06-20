05d719d8603d199d09efd4bbc65143c3d2f22215
Pratyush Mishra <pratyushm206@gmail.com>
2026-06-20 23:29:29 +0530

    feat: add JWT auth middleware to protect sessions route

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const protect = require('../middleware/auth');
 
-router.post('/', (req, res) => {
-    res.json({ message: 'Sessions endpoint' });
+router.post('/', protect, (req, res) => {
+    res.json({ success: true, message: 'Sessions endpoint', user: req.user });
 });
-
 
 module.exports = router;
